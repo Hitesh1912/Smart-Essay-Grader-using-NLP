d928f21c12031da6c8068463a6bbbcce68c3e1f8
Ayush Shukla <ashukla2705@gmail.com>
2019-03-15 01:42:50 -0400

    Corpus cleaning and essay dictionary creation

--- preprocessing.py
+++ preprocessing.py
@@ -9,6 +9,7 @@ from sklearn import preprocessing
 
 
 
+
 # Function importing Dataset
 def importdata():
     # train_data = pd.read_csv(
@@ -38,6 +39,7 @@ def normalization(x,mu,sigma):
     return x
 
 def preprocess_data(training_data):
+    dict_of_scores = dict()
     #split data till essay column
     # data = splitdataset(training_data)
     essay = []
@@ -48,9 +50,11 @@ def preprocess_data(training_data):
 
 def create_essay_chunks(list_of_essays):
     essay_counter = 1
+    dict_of_essays = dict()
     dict_of_essays_with_chunks = dict()
     list_of_essays.pop(0)
     for essay in list_of_essays:
+        dict_of_essays[essay_counter] = essay
         list_of_chunks = list()
         list_of_sentences = essay.split(". ")
         for sentence in list_of_sentences:
@@ -58,12 +62,35 @@ def create_essay_chunks(list_of_essays):
             list_of_chunks.append(clean_sentence)
         dict_of_essays_with_chunks[essay_counter] = list_of_chunks
         essay_counter += 1
-    return dict_of_essays_with_chunks
+    return dict_of_essays_with_chunks, dict_of_essays
+
+def create_list_of_scores(y_train):
+    list_of_scores = list()
+    for row in y_train:
+        list_of_scores.append(row[2])
+    return list_of_scores
+
+def create_dict_of_essays(x_train):
+    counter = 1
+    dict_of_essays = dict()
+    for row in x_train:
+        print(row[2])
+        dict_of_essays[counter] = row[2]
+        counter += 1
+    return dict_of_essays
+
 
 def write_chunked_essay_to_file(dict_of_essays_with_chunks):
-    wf = open("dict_of_chunked_essays.txt", "w")
+    wf = open("dict_of_chunked_essays2.txt", "w")
     wf.write(str(dict_of_essays_with_chunks))
 
+def write_scorelist_to_file(list_of_scores):
+    wf = open("list_of_scores.txt", "w")
+    wf.write(str(list_of_scores))
+
+def write_essaydict_to_file(dict_of_essays):
+    wf = open("dict_of_essays.txt", "w")
+    wf.write(str(dict_of_essays))
 
 def clean_text(text):
     ## Remove puncuation
@@ -106,34 +133,54 @@ def clean_text(text):
     text = re.sub(r"e - mail", "email", text)
     text = re.sub(r"j k", "jk", text)
     text = re.sub(r"\s{2,}", " ", text)
+    text = text.replace(",", "")
+    text = text.replace("!", "")
+    text = text.replace(":", "")
+    text = text.replace(";", "")
+    text = text.replace(".", "")
     ## Stemming
-    text = text.split()
-    stemmer = nltk.SnowballStemmer('english')
-    stemmed_words = [stemmer.stem(word) for word in text]
-    text = " ".join(stemmed_words)
+    # text = text.split()
+    # stemmer = nltk.SnowballStemmer('english')
+    # stemmed_words = [stemmer.stem(word) for word in text]
+    # text = " ".join(stemmed_words)
 
     return text
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
     start_time = time.time()
+    dict_of_scores = dict()
+    dict_of_essays = dict()
     training_data = importdata()
-    X_train, y_train = splitdataset(training_data)
-
+    x_train, y_train = splitdataset(training_data)
+    scaler = preprocessing.MinMaxScaler()
+    normalizer = scaler.fit(y_train)
+    normalized_score = scaler.transform(y_train)
     #normalize all the scores in y_train
-    normalizer = preprocessing.StandardScaler()
-    normalized_score = normalizer.fit_transform(y_train)
+    # normalizer = preprocessing.StandardScaler()
+    # normalized_score = normalizer.fit_transform(y_train)
 
-    # print(training_data[1,0:])
-    list_of_essays = preprocess_data(training_data)
-    dict_of_essays_with_chunks = create_essay_chunks(list_of_essays)
-    write_chunked_essay_to_file(dict_of_essays_with_chunks)
+    essays = x_train[:, 2]
 
+    # print(essays[:3])
+    counter = 1
+    processed_essays = []
+    for essay in essays:
+        processed_essays.append(clean_text(essay))
+
+    for essay in processed_essays:
+        dict_of_essays[counter] = essay
+        counter = counter + 1
+
+
+    # list_of_essays = preprocess_data(training_data)
+    # print(list_of_essays, "%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    # dict_of_essays_with_chunks, dict_of_essays = create_essay_chunks(list_of_essays)
+    list_of_scores = create_list_of_scores(normalized_score)
+    # dict_of_essays = create_dict_of_essays(x_train)
+    # print(dict_of_essays)
+    # write_chunked_essay_to_file(dict_of_essays_with_chunks)
+    # write_scorelist_to_file(list_of_scores)
+    write_essaydict_to_file(dict_of_essays)
     end_time = time.time()
     print("time",end_time - start_time)
